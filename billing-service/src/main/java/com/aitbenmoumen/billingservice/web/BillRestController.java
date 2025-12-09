1dbebc7460f2f5d3ab98580bd3c6355482519d82
aitbenmoumen <a.aitbenmoumen@gmail.com>
2025-12-09 22:01:06 +0100

    a stable point 09/12

--- billing-service/src/main/java/com/aitbenmoumen/billingservice/web/BillRestController.java
+++ billing-service/src/main/java/com/aitbenmoumen/billingservice/web/BillRestController.java
@@ -5,23 +5,22 @@ import com.aitbenmoumen.billingservice.feign.CustomerRestClient;
 import com.aitbenmoumen.billingservice.feign.ProductRestClient;
 import com.aitbenmoumen.billingservice.repositories.BillRepository;
 import com.aitbenmoumen.billingservice.repositories.ProductItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BillRestController {
-    BillRepository billRepository;
-    ProductItemRepository productItemRepository;
-    CustomerRestClient customerRestClient;
-    ProductRestClient productRestClient;
-    public BillRestController(BillRepository billRepository, ProductItemRepository productItemRepository,
-                              CustomerRestClient customerRestClient, ProductRestClient productRestClient) {
-        this.billRepository = billRepository;
-        this.productItemRepository = productItemRepository;
-        this.customerRestClient = customerRestClient;
-        this.productRestClient = productRestClient;
-    }
+    @Autowired
+    private BillRepository billRepository;
+    @Autowired
+    private ProductItemRepository productItemRepository;
+    @Autowired
+    private CustomerRestClient customerRestClient;
+    @Autowired
+    private ProductRestClient productRestClient;
+
     @GetMapping(path = "/bills/{id}")
     public Bill getBill(@PathVariable Long id){
         Bill b = billRepository.findById(id).get();
